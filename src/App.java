c0ceefa5c46dc32baa7d5105d437b257b124298c
Megan <me6383an-s@student.lu.se>
2024-11-09 19:06:24 +0100

    added a feature

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Lets upload this test to github");
+
+        System.out.println("Experimental feature");
     }
 
-    
+
 }
